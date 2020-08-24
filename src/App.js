ab231e38046b83aafa9c9ab2a7422004a226aea1
Matthew Craig <mgcraig78@gmail.com>
2020-08-24 16:23:10 -0700

    add logo with tilt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation' 
+import Logo from './components/logo/Logo' 
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/*<ImageLinkForm />
       <FaceRecognition />*/}
     </div>
   );
