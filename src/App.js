38e51e09817a18e195df3f6a564cc71c6391467a
Matthew Craig <mgcraig78@gmail.com>
2020-08-25 14:38:48 -0700

    add imagelinkform and css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation' 
 import Logo from './components/logo/Logo' 
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm' 
+import Rank from './components/rank/Rank' 
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
+      <ImageLinkForm />
+      <Rank />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
