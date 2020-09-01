c65bf6eee719edb309a170623e7584fa7acf8126
Matthew Craig <mgcraig78@gmail.com>
2020-09-01 15:43:43 -0700

    trouble shoot math and create css for drawing bouding box around face

--- src/App.js
+++ src/App.js
@@ -133,8 +133,26 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
@@ -144,16 +162,10 @@ class App extends Component {
     app.models
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
-      this.state.input).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      // do something with response
-    },
-    function(err) {
-      // there was an error
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response))) 
+      .catch(err => console.log(err))
     }
-  );
-  }
   render(){
   return (
     <div className="App">
@@ -166,10 +178,9 @@ class App extends Component {
       <ImageLinkForm 
       onInputChange={this.onInputChange} 
       onSubmit={this.onSubmit} />
-      <FaceRecognition
+      <FaceRecognition box={this.state.box}
       imageUrl={this.state.imageUrl} />
     </div>
   )}
-}
-
+  }
 export default App;
